dd1dd3f451c9ba29a985b88198394511d9592d7a
Yujie <yujie@clear-measure.com>
2016-02-18 14:59:55 -0600

    Add REST Endpoint with no parameters

---
+++ Heartbeat/API/Controllers/PingController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("ping")]
+    public class PingController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
