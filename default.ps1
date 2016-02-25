120ac27a47faeab2179ff15a4eedf3ff1bd5eff3
Matt <matt.sonnhalter@clear-measure.com>
2016-02-25 17:50:12 -0600

    Added default tasks for build script

---
+++ default.ps1
@@ -0,0 +1,21 @@
+﻿properties {
+    $baseDir = Resolve-Path .\
+    $config = "Debug"
+    $srcDir = "$baseDir\Heartbeat\Heartbeat.sln"
+    $VSversion = "/p:VisualStudioVersion=14.0"
+    }
+
+task -name Clean -description "Deletes all build artifacts" -action {
+    exec { 
+            msbuild $srcDir /t:Clean $VSversion
+        }
+}
+
+task -name Build -description "Builds the outdated artifacts" -action { 
+    exec { 
+            msbuild $srcDir /t:Build $VSversion
+        }
+}
+
+task -name default -depends Clean, Build -description "Cleans and Builds project"
+
