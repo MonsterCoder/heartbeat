7c4ffa4e1fe9ac62472420eec3154966c0ee0935
cakeller43 <cakeller43@gmail.com>
2016-02-26 16:19:10 -0600

    Fix the package restore section of the build script.
    Also add a tools directory to store psake and nuget.exe

--- default.ps1
+++ default.ps1
@@ -8,22 +8,21 @@
 
 task -name PackageRestore -description "Restores nuget packages" -action {
     exec {
-            nuget.exe restore $srcDir
+            .\tools\nuget.exe restore $srcDir
         }
 }
 
 task -name Clean -depends PackageRestore -description "Deletes all build artifacts" -action {
-    exec { 
+    exec {
             msbuild $srcDir /t:Clean $VSversion
            # remove-item $packagesDir -recurse -ErrorAction Ignore
         }
 }
 
-task -name Build -description "Builds the outdated artifacts" -action { 
-    exec { 
+task -name Build -description "Builds the outdated artifacts" -action {
+    exec {
             msbuild $srcDir /t:Build $VSversion
         }
 }
 
 task -name default -depends Clean, Build -description "Cleans and Builds project"
-
